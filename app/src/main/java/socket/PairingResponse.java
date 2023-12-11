d87c63178b5db728361fdaf753ef1c0644fd23b8
aucancea <148291128+aucancea@users.noreply.github.com>
2023-12-11 15:17:42 -0500

    Update PairingResponse.java

--- app/src/main/java/socket/PairingResponse.java
+++ app/src/main/java/socket/PairingResponse.java
@@ -1,1 +1,49 @@
+package socket; 
+import model.User;
+import model.Event;
+import java.util.List;
 
+public class PairingResponse extends Response {
+    private List<User> availableUsers;
+    private Event invitation;
+    private Event invitationResponse;
+  
+  
+  public PairingResponse() {
+    super();
+  }
+  
+  
+  public PairingResponse(ResponseStatus status, String message, List<User> availableUsers, Event invitation, Event invitationResponse) {
+    super(status, message);
+    this.availableUsers = availableUsers;
+    this.invitation = invitation;
+    this.invitationResponse = invitationResponse;
+  }
+
+
+  public List<User> getAvailableUsers() {
+    return availablesers;
+  }
+
+  public void setAvailableUsers(List<User> availableUsers) {
+    this.availableUsers = availableUsers;
+  }
+
+  public Event getInvitation() {
+    return invitation;
+  }
+
+  public void setInvitation (Event invitation) {
+    this.invitation = invitation;
+  }
+
+  public Event getInvitationResponse() {
+    return invitationResponse; 
+  }
+
+  public void setInvitationResponse (Event invitationResponse) {
+    this.invitationResponse = invitationResponse;
+  }
+  
+}
